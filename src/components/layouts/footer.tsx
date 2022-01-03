7f4be6ec3090b4c33b26f519d1d45159cb8d0048
zhoufel1 <felix.zhou@mail.utoronto.ca>
2022-01-03 04:11:24 -0500

    work on header footer

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -1,17 +1,77 @@
 import React from 'react';
 import { styled } from 'src/lib/styles';
+import Separator from 'src/components/separator';
+import ATag from '../a-tag';
+import Link from 'next/link';
 
-const _Footer = styled('div', {
+const _Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  paddingBottom: 25,
+  paddingTop: 75,
+  gap: 10,
+});
+const _Row = styled('div', {
   display: 'flex',
   justifyContent: 'center',
   width: '100vw',
+  fontFamily: '$code',
+  gap: 10,
+  alignItems: 'center',
+});
+
+const _Text = styled('p', {
+  fontSize: 13,
+  margin: 2,
+  variants: {
+    fontWeight: {
+      bold: {
+        fontWeight: 'bolder',
+      },
+    },
+  },
 });
 
 const Footer = () => {
   return (
-    <_Footer>
-      <p>{'\u00A9 '} 2022</p>
-    </_Footer>
+    <_Container>
+      <_Row>
+        <_Text>{'\u00A9 '} 2022 Felix Z</_Text>
+        <Separator orientation="vertical" />
+        <_Text fontWeight="bold">ce15aff</_Text>
+        <Separator orientation="vertical" />
+        <Link href="/">
+          <ATag padding="none" fontSize="sm">
+            Site Map
+          </ATag>
+        </Link>
+      </_Row>
+      <_Row>
+        <Link href="/">
+          <ATag padding="none" fontSize="sm">
+            Icon 1
+          </ATag>
+        </Link>
+        <Separator orientation="vertical" />
+        <Link href="/">
+          <ATag padding="none" fontSize="sm">
+            Icon 2
+          </ATag>
+        </Link>
+        <Separator orientation="vertical" />
+        <Link href="/">
+          <ATag padding="none" fontSize="sm">
+            Icon 3
+          </ATag>
+        </Link>
+        <Separator orientation="vertical" />
+        <Link href="/">
+          <ATag padding="none" fontSize="sm">
+            Icon 4
+          </ATag>
+        </Link>
+      </_Row>
+    </_Container>
   );
 };
 
--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import { styled } from 'src/lib/styles';
+import Button from '../button';
+import ATag from '../a-tag';
+import Separator from '../separator';
+import Link from 'next/link';
+
+const _Container = styled('div', {
+  paddingBottom: 25,
+});
+
+const _Stack1 = styled('div', {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+
+const _Stack = styled('div', {
+  display: 'flex',
+  gap: 25,
+});
 
 const Header = () => {
   return (
-    <>
-      <h1>BANNER</h1>
-    </>
+    <_Container>
+      <_Stack1>
+        <h1>BANNER</h1>
+        <Button>TEST</Button>
+      </_Stack1>
+      <Separator />
+      <_Stack>
+        <Link href="/test">
+          <ATag>ABOUT</ATag>
+        </Link>
+        <Link href="/test">
+          <ATag>BLOG</ATag>
+        </Link>
+        <Link href="/test">
+          <ATag>PROJECTS</ATag>
+        </Link>
+        <Link href="/test">
+          <ATag>RESUME</ATag>
+        </Link>
+      </_Stack>
+    </_Container>
   );
 };
 
