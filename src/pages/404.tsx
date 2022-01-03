00e909a523f02dfef1c7e9b4c1718bee2c1a8a25
zhoufel1 <felix.zhou@mail.utoronto.ca>
2022-01-03 04:10:41 -0500

    owo

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { styled } from 'src/lib/styles';
+
+const _Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '50vh',
+});
 
 const Custom404Page = () => {
   return (
     <>
-      <h1>Fucko Wucko</h1>
+      <_Container>
+        <h3>You made a Fucko Wucko</h3>
+        <h1>O W O</h1>
+      </_Container>
     </>
   );
 };
