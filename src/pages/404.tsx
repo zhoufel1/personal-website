43ee3d1b6a28431eea04f390c3874bd3e4d3d63d
zhoufel1 <felix.zhou@mail.utoronto.ca>
2022-03-21 00:21:06 -0400

    work on 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import { styled } from 'src/lib/styles';
+import Container from 'src/components/primitives/container';
+import Separator from 'src/components/separator';
 
-const _Container = styled('div', {
-  display: 'flex',
-  flexDirection: 'column',
+const _Container = styled(Container, {
   justifyContent: 'center',
   alignItems: 'center',
-  height: '50vh',
+  height: '35vh'
+});
+
+const _Stack = styled('div', {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'baseline',
+  width: 175
 });
 
 const Custom404Page = () => {
   return (
     <>
       <_Container>
-        <h1>O W O</h1>
+        <_Stack>
+          <h1>404</h1>
+          <h5>Page Not Found</h5>
+        </_Stack>
       </_Container>
     </>
   );
